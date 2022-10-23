d8d4ba8d8ef8a2ecc6d0f7e81f5ed8621fbadb01
Jacob Foshee <jacobf@gmail.com>
2022-10-23 13:47:20 -0500

    test: Use hosted dev server instance

--- GameMaster.Tests/TicTacToeTestAttribute.cs
+++ GameMaster.Tests/TicTacToeTestAttribute.cs
@@ -2,5 +2,9 @@ namespace GameMaster.Tests;
 
 public class TicTacToeTestAttribute : GameTestAttribute
 {
-    public TicTacToeTestAttribute() : base("tic-tac-toe-game-master") { }
+    public TicTacToeTestAttribute() : base("tic-tac-toe-game-master")
+    {
+        // Hosted Dev instance (matches dev instance in tasks.json)
+        BaseUrl = "https://app-persisted-world-dev.azurewebsites.net";
+    }
 }
